9dc587bde957a1d86a25c3f35b565a1a019c79c9
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2019-01-18 10:30:19 -0700

    Update Island.java

--- Island.java
+++ Island.java
@@ -2,8 +2,52 @@ import java.util.Random;
 
 public class Island 
 {
+	public Island(){
+		
+		//1 loop and inside thos loops u ned 5 if staments
+		
+		for(int i = 0; i < 15; i++)
+		{
+			
+			//generate rand num
+			int randInt = generator.nextInt(100) + 1;
+			
+			//check if less or equal to PreCENT constant
+			//if it is then say island[0][i] = -1;
+			if(randInt <= PERCENT_BRIDGES)
+			{
+				island[0][i] = -1;				
+			}
+			else
+			{
+				island[9][i] = -1;
+			}
+			
+			if(i <= 8)
+			{
+				
+				if(randInt <= PERCENT_BRIDGES)
+				{
+					island[i][0] = -1;				
+				}
+				else
+				{
+					island[i][14] = -1;
+				}
+				
+			}
+			
+			//printIsland();
+			
+			//another if stament same thing but greater than or equal to if so then island[9][i] = -1
+		}
+		
+		
+		printIsland();
+		
+	}
 	
-	public String printIsland()
+	public void printIsland()
 	{
 		
 	    String r = "";
@@ -13,59 +57,101 @@ public class Island
 			//r = r + "";
 	         for (int j = 0; j < COLUMNS; j++)
 	         {
+	        	
+	        	 //int randInt = 1 + generator.nextInt(100);
 	        	 
-	        	int randInt = generator.nextInt(100);
-	        	if(randInt <= PERCENT_BRIDGES)
-	        	{
-	        		island[j][0] = 0;
-	        	}
-	        	
-	        	
-	        	/*
-	        	int checkX = generator.nextInt(9);
-	        	int checkY = generator.nextInt(14);
-	        	
-	        	if(checkX > 0 && checkY > 0)
-	        	{
-	        		island[checkX][checkY] = 0;
-	        		r = r + island[checkX][checkY];
-	        		
-	        	}
-	        	
-	        	else if(checkX == 0 || checkX == 9 || checkY == 0 || checkY == 14)
-	        	{
-	        		
-	        		randInt = generator.nextInt(100);
-	        		if(randInt<=PERCENT_BRIDGES)
-	        		{
-	        			island[checkX][checkY] = 0;
-	        			r = r + island[checkX][checkY];
-		        		
-	        		}
-	        		else
-	        		{
-	        			island[checkX][checkY] = -1;
-	        			r = r + island[checkX][checkY];
-		        		
-	        		}
-	        		
-	        	}
-	        	*/
-	        	
-	        	//r = r + island[checkX][checkY];
-	        	
+	        	 if(i == 0 || i == 9 || j == 0 || j == 14)
+	        	 {
+	        		 if(j == 0 && (i!=0 && i!=9))
+	        		 {
+	        			 
+	        			 if(island[i][j] == -1)
+		        		 {
+		        			 
+		        			 System.out.print(" W");	        			 
+		        		 }
+		        		 if(island[i][j] == 0)
+		        		 {
+		        			 
+		        			 System.out.print(" B");
+		        			 
+		        		 }
+	        			 
+	        		 }
+	        		 
+	        		 else if (j == 14 && (i!=0 && i!=9))
+	        		 {
+	        			 
+	        			 if(island[i][j] == -1)
+		        		 {
+		        			 
+		        			 System.out.print("  W");	        			 
+		        		 }
+		        		 if(island[i][j] == 0)
+		        		 {
+		        			 
+		        			 System.out.print("  B");
+		        			 
+		        		 }
+	        			 
+	        		 }
+	        		 
+	        		 else
+	        		 {
+	        			
+	        			 if(island[i][j] == -1)
+		        		 {
+		        			 
+		        			 System.out.print(" W ");	        			 
+		        		 }
+		        		 if(island[i][j] == 0)
+		        		 {
+		        			 
+		        			 System.out.print(" B ");
+		        			 
+		        		 }
+	        			 
+	        		 }
+	        		 
+	        		 
+	        		 continue;
+	        	 }
+	        	 
+	        	 
+	        	 
+	        	 System.out.print(String.format("%3d", island[i][j]));
+	        	 
 	         }
+	         
+	         
+	        System.out.println();
 		
-	         r = r + "\n";
+	         //r = r + "\n";
 	         
 		}
 		
-		return r;
+		//System.out.println();
 		
 	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public String moveAbout()
 	{
+		
+		boolean searching = true;
+		boolean escaped = false;
+		boolean drowned = false;
+		boolean starved = false;
+		
+		
 		
 		return null;
 		
