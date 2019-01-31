66abdd9503278883c7ba20ae6a1ed681ecdf6da9
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2019-01-31 10:30:52 -0700

    Update IslandTest.java

--- IslandTest.java
+++ IslandTest.java
@@ -6,7 +6,29 @@ public class IslandTest
 	{
 		
 		Island island = new Island();
+
+		int x = 0;
+		int y = 0;
+		int z = 0;
+		String s;
 		
+		for(int i = 1; i < 1000; i++){
+			
+		island = new Island();
+		s = island.moveAbout();
+		if(s.charAt(0) == 'D'){
+			y++;
+		}
+		else if(s.charAt(0) == 'S'){
+			z++;
+		}
+		else if(s.charAt(0) == 'E'){
+			x++;
+		}
+		
+		
+		}
+		island.printStats(x, y, z);
 	}
 	
 }
